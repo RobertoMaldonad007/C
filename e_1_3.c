e8920406418b0e711b71bbb44d42077993f89109
RobertoMaldonad007 <roberto.alexis@outlook.com>
2021-06-03 19:20:28 -0500

    Ejercicio 1-3

--- e_1_3.c
+++ e_1_3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+
 /* imprime la tabla Fahrenheit-Celsius
-    para fahr = 0,20, ..., 300 */
+    para fahr = 0,20, ..., 300; versión de punto flotante. */
 int main()
 {
 	printf("Tabla de conversiones (Fahrenheit-Celsius)\n\n");
@@ -8,12 +9,12 @@ int main()
 	float fahr, celsius;
 	int lower, upper, step;
 	
-	lower = 0;           /* l�mite inferiro de la tabla de temperaturas */
-	upper = 300; /* l�mite superior */
-	step = 20;           /* tama�o del incremento */
+	lower = 0;		/* límite inferior de la tabla de temperaturas */
+	upper = 300;	/* límite superior */
+	step = 20;		/* tamaño del incremento */
 	
 	fahr = lower;
-	while (fahr >= upper) {
+	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
