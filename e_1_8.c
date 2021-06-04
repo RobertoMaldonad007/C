681ad5bd5a8fc8f2384c6f12c67cab6053fcdab7
RobertoMaldonad007 <78567095+RobertoMaldonad007@users.noreply.github.com>
2021-06-03 23:00:57 -0500

    Ejercicio 1-8

--- e_1_8.c
+++ e_1_8.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
 
-/* count lines in input */
-main()
+int main()
 {
-	double c, n1, n2, n3;
+	int c, n1, n2, n3;
 	
-	n1 = 0;
-	n2 = 0;
-	n3 = 0;
+	n1 = n2 = n3 =0;
 	while((c = getchar()) != EOF)
 		if(c == '\n')
-			++n1;
+			n1++;
 		else if(c == '\t')
-			++n2;
+			n2++;
 		else if(c == ' ')
-			++n3;
-	printf("Cantidad de lineas: %.f\n", n1);
-	printf("Cantidad de tabuladores: %.f\n", n2);
-	printf("Cantidad de espacios en blanco: %.f\n", n3);
+			n3++;
+	printf("Number of new lines: %.d\nNumber of tabs: %d.\nNumber of blanks: %d.", n1,n2,n3);
 }
